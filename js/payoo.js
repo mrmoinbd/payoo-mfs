1adacbbda1f285f58c90c1cd5bff6895881d46e8
Moin Uddin <moinuddin.siu.bd@gmail.com>
2025-02-05 04:53:42 +0600

    add login page

--- js/payoo.js
+++ js/payoo.js
@@ -5,4 +5,17 @@ document.getElementById('btn-login').addEventListener('click', function(event){
     const phoneNumber = document.getElementById('phone-number').value;
     console.log(phoneNumber);
 
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(pinNumber);
+
+    if(phoneNumber === '5' && pinNumber === '1234'
+    ){
+        console.log('you are login this path')
+        window.location.href= ("./home.html");
+    }
+    else{
+        alert('Your phone number or password is wrong')
+    }
+
+
 })
