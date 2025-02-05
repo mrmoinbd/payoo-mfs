7e27919d62cb75ee1150406624701c68a2121041
Moin Uddin <moinuddin.siu.bd@gmail.com>
2025-02-05 12:48:55 +0600

    added a event hander

--- js/home.js
+++ js/home.js
@@ -1,1 +1,9 @@
-console.log('home Test');
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+   event.preventDefault();
+
+   const addMoney = document.getElementById('input-add-money').value;
+   console.log(addMoney);
+
+   const addPin = document.getElementById('input-pin').value;
+   console.log(addPin);
+})
