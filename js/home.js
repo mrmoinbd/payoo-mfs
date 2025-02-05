9f324f3621ecbe0f29ff6aaab114d5cf44e1617e
Moin Uddin <moinuddin.siu.bd@gmail.com>
2025-02-05 14:29:39 +0600

    added addmoney button functionality

--- js/home.js
+++ js/home.js
@@ -6,4 +6,21 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
 
    const addPin = document.getElementById('input-pin').value;
    console.log(addPin);
-})
+
+   if(addPin === '1234'){
+    console.log('Add money to main balance');
+
+    const mainBalance = document.getElementById('balance-money').innerText;
+    // console.log(mainBalance)
+
+  const mainBalanceNumber = parseFloat(mainBalance);
+  const addMoneyNumber = parseFloat(addMoney);
+  const newBalance = mainBalanceNumber + addMoneyNumber;
+  console.log(newBalance);
+
+  document.getElementById('balance-money').innerText = newBalance;
+   }
+   else{
+    alart('Wrong number');
+   }
+});
